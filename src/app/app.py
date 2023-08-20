76c3df519bd144f6e3c2bb6b0a499a9254269358
Vítor Fróis <vitor0frois@gmail.com>
2023-08-20 20:15:34 -0300

    new changes

--- src/app/app.py
+++ src/app/app.py
@@ -5,26 +5,26 @@ from dash import dcc
 from dash import html
 import plotly.graph_objects as go
 import pandas as pd 
-# import mysql.connector
+import psycopg2
 
 app = dash.Dash(__name__)
 
 # Função para aguardar o banco de dados estar pronto
 def wait_for_db():
-    max_attempts = 10
+    max_attempts = 1
     attempts = 0
     while attempts < max_attempts:
         try:
             # Tenta conectar ao banco de dados
-            mydb = mysql.connector.connect(
+            mydb = psycopg2.connect(
                 host=os.environ.get('MYSQL_HOST'),
                 user=os.environ.get('MYSQL_USER'),
                 password=os.environ.get('MYSQL_ROOT_PASSWORD'),
-                database=os.environ.get('MYSQL_DATABASE')
+                database=os.environ.get('MYSQL_DATABASE'),
+                port=os.environ.get('POSTGRES_PORT')
             )
-            mydb.close()
-            break  # Conexão bem-sucedida, sai do loop
-        except mysql.connector.Error as err:
+            return mydb
+        except Exception as err:
             print(f"Erro ao conectar ao banco de dados: {err}")
             attempts += 1
             time.sleep(10)  # Aguarda 5 segundos antes de tentar novamente
@@ -32,14 +32,7 @@ def wait_for_db():
         # Caso atinja o limite de tentativas, exibe uma mensagem de erro
         print("Não foi possível conectar ao banco de dados após várias tentativas.")
 
-wait_for_db()
-
-mydb = mysql.connector.connect(
-    host=os.environ.get('MYSQL_HOST'),
-    user=os.environ.get('MYSQL_USER'),
-    password=os.environ.get('MYSQL_ROOT_PASSWORD'),
-    database=os.environ.get('MYSQL_DATABASE')
-)
+mydb = wait_for_db()
 
 cursor = mydb.cursor()
 
