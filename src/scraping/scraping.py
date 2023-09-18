834e53e079aa7a15d6c840ddd82039ec367f1655
Vítor Fróis <vitor0frois@gmail.com>
2023-09-18 15:36:13 -0300

    refactored scraping script

--- src/scraping/scraping.py
+++ src/scraping/scraping.py
@@ -3,15 +3,13 @@ import os
 import time
 
 from tqdm import tqdm
+from dataclasses import dataclass
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-# from selenium.webdriver.chrome.options import Options as ChromeOptions
-# from selenium.webdriver.chrome.service import Service
-# from webdriver_manager.chrome import ChromeDriverManager
 import traceback
 import psycopg2
 
@@ -39,41 +37,55 @@ def wait_for_db():
     # Caso atinja o limite de tentativas, exibe uma mensagem de erro
     print("Não foi possível conectar ao banco de dados após várias tentativas.")
 
+@dataclass
+class ReviewInfo:
+    text: str
+    rating: int
+    title: str
+    author: str
+    date: str
+
 
 class DatabaseHelper:
     def __init__(self):
         self.mydb = wait_for_db()
         self.cursor = self.mydb.cursor()
 
-    def insert_review(self, table_suffix, review_rating, review_title, review_author, review_date, review_text,
-                      title_id):
+    def insert_review(self, table_suffix: str, title_id, review: ReviewInfo):
         self.cursor.execute(
             f'INSERT INTO IMDB_Reviews_{table_suffix} \
             (review_rating, review_title, review_author, review_date, review_text, id_title) \
-            VALUES (%s, %s, %s, %s, %s, %s)', \
-            (review_rating, review_title, review_author, review_date, review_text, title_id)
+            VALUES (%s, %s, %s, %s, %s, %s)',
+            (review.rating, review.title, review.author, review.date, review.text, title_id)
         )
 
     def query_title(self, title_name, n_reviews, table_suffix):
         title_name = re.sub(r'\'', "''", title_name)
         print(table_suffix, title_name)
-        id_title = list(self.cursor.execute(f"SELECT id_title FROM IMDB_{table_suffix} WHERE title='{title_name}'"))
-        if len(id_title) == 0:
-            result = self.cursor.execute(
+        self.cursor.execute(f"SELECT id_title FROM IMDB_{table_suffix} WHERE title='{title_name}'")
+        id_title = self.cursor.fetchone()
+        if id_title is None:
+            self.cursor.execute(
                 f'INSERT INTO IMDB_{table_suffix} (title, n_reviews) VALUES (%s, %s) RETURNING id_title',
                 (title_name, n_reviews))
-            id_title = result.fetchone()[0]
+            id_title = self.cursor.fetchone()[0]
         else:
             id_title = id_title[0][0]
         return id_title
 
+    def commit_changes(self):
+        self.mydb.commit()
+
+    def close_connection(self):
+        self.cursor.close()
+        self.mydb.close()
 
 class ImdbScraper:
     driver = None
     mydb = None
 
     def __init__(self):
-        driver, wait = init_driver()
+        driver, wait = self.init_driver()
         self.driver = driver
         self.wait = wait
         self.db_helper = DatabaseHelper()
@@ -92,7 +104,7 @@ class ImdbScraper:
         wait = WebDriverWait(driver, 10)
         return driver, wait
 
-    def get_total_reviews(self):
+    def get_number_reviews(self) -> int:
         sort_reviews = self.wait.until(EC.element_to_be_clickable((By.XPATH, '//option[@value="submissionDate"]')))
         sort_reviews.click()
 
@@ -101,11 +113,10 @@ class ImdbScraper:
         number_reviews = number_reviews.split()[0]
         if ',' in number_reviews:
             number_reviews = number_reviews.replace(',', '')
-        total_reviews = int(number_reviews)
 
-        return total_reviews
+        return int(number_reviews)
 
-    def load_reviews_list(self, total_reviews):
+    def fetch_reviews_info_list(self, total_reviews) -> list:
         if total_reviews > NUM_REVIEWS_PER_LOAD:
             n_load_more = total_reviews // NUM_REVIEWS_PER_LOAD
             n_load_more = 1
@@ -121,22 +132,31 @@ class ImdbScraper:
 
         review_list = self.wait.until(
             EC.visibility_of_all_elements_located((By.XPATH, '//div[@class="lister-item-content"]')))
-        return review_list
+
+        review_info_list = self.fetch_reviews_info(review_list)
+
+        return review_info_list
 
     @staticmethod
     def pre_process_text(text):
         preprocessed_text = re.sub(r'\n+', '\n', text)
         preprocessed_text = re.sub(r'http\S+', '', text)  # removendo links
         preprocessed_text = preprocessed_text.replace('"', '')  # removendo aspas
-        preprocessed_text = re.sub(r"<\S*\ ?\/?>", '', preprocessed_text)
+        preprocessed_text = re.sub(r"<\S* ?/?>", '', preprocessed_text)
         preprocessed_text = re.sub("[-*!,$><:.+?=]", '', preprocessed_text)  # remove outras pontuações
 
         preprocessed_text = re.sub(r'[.]\s+', '', preprocessed_text)  # removendo reticências 
-        preprocessed_text = re.sub(r'  ', ' ', preprocessed_text)  # removendo espaços extras
+        preprocessed_text = re.sub(r' ', ' ', preprocessed_text)  # removendo espaços extras
         preprocessed_text = re.sub(r'\'', "''", preprocessed_text)
         return preprocessed_text.lower()
 
-    def get_review_info_and_insert(self, reviews_list, title_id, table_suffix):
+    def insert_review_info(self, reviews_info, title_id, table_suffix):
+        for review_info in reviews_info:
+            self.db_helper.insert_review(
+                table_suffix, title_id, review_info)
+
+    def fetch_reviews_info(self, reviews_list):
+        reviews_info: list[ReviewInfo] = []
         for review in reviews_list:
             try:
                 container_content = review.find_element(By.CLASS_NAME, 'content')
@@ -144,43 +164,41 @@ class ImdbScraper:
                 review_text = self.pre_process_text(review_text)
                 if review_text == '':
                     continue
-            except:
+            except Exception:
                 review_text = None
 
             try:
                 container_span = review.find_element(By.CLASS_NAME, 'rating-other-user-rating')
                 span = container_span.find_elements(By.TAG_NAME, 'span')[0]
                 review_rating = span.text
-            except:
+            except Exception:
                 review_rating = None
 
             try:
                 review_title = review.find_element(By.CLASS_NAME, 'title').text
                 review_title = re.sub(r'\'', "''", review_title)
-            except:
+            except Exception:
                 review_title = None
 
             try:
                 container_author = review.find_element(By.CLASS_NAME, 'display-name-date')
                 review_author = container_author.find_element(By.TAG_NAME, 'a').text
                 review_author = re.sub(r'\'', "''", review_author)
-            except:
+            except Exception:
                 review_author = None
 
             try:
                 review_date = container_author.find_element(By.CLASS_NAME, 'review-date').text
-            except:
+            except Exception:
                 review_date = None
 
-            self.db_helper.insert_review(table_suffix, review_rating, review_title, review_author, review_date,
-                                         review_text, title_id)
+            reviews_info.append(
+                ReviewInfo(review_text, review_rating, review_title, review_author, review_date)
+            )
 
-    def imdb_scrape_reviews(self):
-        total_reviews = self.get_total_reviews()
-        reviews_list = self.load_reviews_list(total_reviews)
-        return reviews_list
+        return reviews_info
 
-    def fetch_title_links(self):
+    def fetch_titles_links(self):
         try:
             elements = self.wait.until(
                 EC.visibility_of_all_elements_located((By.XPATH, '//a[@class="ipc-title-link-wrapper"]')))
@@ -192,32 +210,34 @@ class ImdbScraper:
             print(e)
             return []
 
-    def imdb_fetch_links(self):
-        self.driver.get('https://www.imdb.com/chart/top/?ref_=nv_mv_250')
-        movies_links = self.fetch_title_links()
-        self.driver.get('https://www.imdb.com/chart/toptv/?ref_=nv_tvv_250')
-        series_links = self.fetch_title_links()
+    def fetch_title_link_list(self, reviews_category_url: str) -> list:
+        self.driver.get(reviews_category_url)
+        elements = self.wait.until(
+            EC.visibility_of_all_elements_located((By.XPATH, '//a[@class="ipc-title-link-wrapper"]')))
+        url_list = [element.get_attribute('href') for element in elements]
+        title_link_list = [url for url in url_list if url.startswith('https://www.imdb.com/title/')]
 
-        if len(movies_links) == 0 or len(series_links) == 0:
-            raise Exception("Could not fetch movies and series links correctly")
+        if len(title_link_list) == 0:
+            raise Exception(f"Could not fetch titles from {reviews_category_url} links correctly")
 
-        print(f'Fetched {len(movies_links)} links for movie titles and {len(series_links)} links for series titles')
-        return movies_links, series_links
+        return title_link_list
 
-    def imdb_scrape(self, urls_list, table_suffix):
+    def fetch_imdb_reviews(self, urls_list, table_suffix):
         for url in tqdm(urls_list):
             try:
                 review_url = re.sub("(\?[A-Za-z0-9\_=&\\\/-]*)", 'reviews', url)
                 print(f'Going to reviews page: {review_url}')
                 self.driver.get(review_url)
                 div_parent = self.wait.until(EC.visibility_of_element_located((By.XPATH, '//div[@class="parent"]')))
-
                 title_name = div_parent.find_element(By.TAG_NAME, 'a').text
 
                 print(f'Retrieving reviews for {table_suffix[:-1].lower()}: {title_name}')
-                reviews = self.imdb_scrape_reviews()
-                title_id = self.db_helper.query_title(title_name, len(reviews), table_suffix)
-                self.get_review_info_and_insert(reviews, title_id, table_suffix)
+                total_reviews = self.get_number_reviews()
+                reviews_info = self.fetch_reviews_info_list(total_reviews)
+
+                title_id = self.db_helper.query_title(title_name, len(reviews_info), table_suffix)
+                self.insert_review_info(reviews_info, title_id, table_suffix)
+
             except Exception as e:
                 print(traceback.format_exc())
                 print('Error fetching reviews')
@@ -225,13 +245,17 @@ class ImdbScraper:
 
     def scrape_reviews(self):
         try:
-            movies_links, series_links = self.imdb_fetch_links()
+            movies_links = self.fetch_title_link_list('https://www.imdb.com/chart/top/?ref_=nv_mv_250')
+            series_links = self.fetch_title_link_list('https://www.imdb.com/chart/toptv/?ref_=nv_tvv_250')
         except Exception as e:
             print(e)
             exit()
 
-        self.imdb_scrape(movies_links, table_suffix='Movies')
-        self.imdb_scrape(series_links, table_suffix='Series')
+        self.fetch_imdb_reviews(movies_links, table_suffix='Movies')
+        self.fetch_imdb_reviews(series_links, table_suffix='Series')
+        self.db_helper.commit_changes()
+        self.db_helper.close_connection()
+        print("IMDB Reviews successfully retrieved and inserted in DB")
 
 
 if __name__ == "__main__":
