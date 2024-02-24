68d03e38d6de172e99b3b573f438144d7cefc869
Vítor Fróis <vitor0frois@gmail.com>
2024-02-24 02:29:12 -0300

    changes to project

--- src/app/app.py
+++ src/app/app.py
@@ -1,8 +1,7 @@
 import os
 import time
 import dash
-from dash import dcc
-from dash import html
+from dash import dcc, html
 import plotly.graph_objects as go
 import pandas as pd 
 import psycopg2
@@ -11,6 +10,17 @@ import numpy
 app = dash.Dash(__name__)
 
 columns = ['admiration','amusement','anger','annoyance','approval','caring','confusion','curiosity','desire','disappointment','disapproval','disgust','embarrassment','excitement','fear','gratitude','grief','joy','love','nervousness','optimism','pride','realization','relief','remorse','sadness','surprise','neutral']
+
+def dict_to_markdown(titles_list: dict, n_titles: int = 3) -> str:
+    md = ''''''
+    for i in range(n_titles):
+        elem = titles_list[i]
+        md += f'**{elem[0]}**: {elem[1]:.2f}  \n\n'
+
+    return md
+    
+
+
 
 # Função para aguardar o banco de dados estar pronto
 def wait_for_db():
@@ -73,6 +83,7 @@ cursor.execute("SELECT title FROM IMDB_Titles")
 titles = cursor.fetchall()
 titles_dict = get_titles_vec(cursor)
 
+
 # Define the dropdown options
 dropdown_options = {'Titles':
                     [{'label': title[0], 'value': title[0]} for title in titles]
@@ -80,10 +91,11 @@ dropdown_options = {'Titles':
 
 app.layout = html.Div(
     children=[
-        html.H1("IMDB reviews classifications", style = {"text-align": "center", "margin":"20px 0px 20px 0px"}),
+        html.H1("Cinema Recommendation based on IMDB", style = {"text-align": "center", "margin":"20px 0px 20px 0px"}),
         html.Div(
             children = [
-                html.P("In this website you are able to select a title that is currently in the top 250 movies or top 250 series in IMDB and check for the overall classification of the reviews from this title in the same page. For this project, we use a BERT based model with finetuning in goemotions dataset (but with resampled set of labels)")
+                html.P("In this website you are able to select a title and check for recomendations based on similarity. The titles were vectorized according to IMDB Reviews sentiment analysis."),
+                html.Img(src='data.jpg')
             ],
             style = {"background":"#fafafa", "width":"80%", "margin": "auto auto", "padding":"40px 40px 40px 40px", "boder-radius": "5px"}
         ),
@@ -101,7 +113,9 @@ app.layout = html.Div(
 
             style = {"display": "flex", "flex-direction": "row", "width": "80%", "justify-content": "space-evenly", "background": "#ffffff", "margin": "40px auto 40px auto", "gap": "10px", "align-items": "stretch"}
         ),
-        html.Div(id='output')  # Output div to display the selected option
+        html.H2("Results", style = {"text-align": "center", "margin":"20px 0px 20px 0px"}),
+        dcc.Markdown(id='output')
+        # html.Div(id='output')  # Output div to display the selected option
         
     ],
     style = {"font-family": "Courier New"}
@@ -123,7 +137,14 @@ def change_dropdown_options(check_option):
 def update_output(n_clicks, selected_first_title):
     distances = get_title_distance(selected_first_title, titles_dict)
     
-    return f"{distances}"
+    distance_list = []
+    for title in distances:
+        distance_list.append((title, 100 * float(distances[title])))
+
+    distance_list.sort(reverse=True, key=lambda a: a[1])
+
+    return dict_to_markdown(distance_list)
+    # return f"{distances}"
 
 if __name__ == "__main__":
     app.run_server(debug=True, host="0.0.0.0", port=8050, use_reloader=False)
